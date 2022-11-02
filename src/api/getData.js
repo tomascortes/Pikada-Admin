411f474c74eb19da600f36f2f8909b05e4dcf991
tomascortes <ticortes@uc.cl>
2022-11-02 00:19:14 -0300

    feat(changed to local database)

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,15 +1,13 @@
-import axios from 'axios';
+import getData from '../database/database.json';
 
-async function getData() {
-  const { data } = await axios({
-    method: 'get',
-    url: 'https://storage.googleapis.com/backupdatadev/ejercicio/ventas.json',
-  });
-  return data;
-}
+// async function getData() {
+//   const data = require('./data.json');
+//   console.log(data);
+//   return data;
+// }
 
-export async function monthDates(month, year) {
-  const orders = await getData();
+export function monthDates(month, year) {
+  const orders = getData;
   const output = [];
   let auxDate;
   for (let i = 0; i < orders.length; i += 1) {
@@ -24,8 +22,8 @@ export async function monthDates(month, year) {
   return output;
 }
 
-export async function totalIncome(month, year) {
-  const orders = await monthDates(month, year);
+export function totalIncome(month, year) {
+  const orders = monthDates(month, year);
   let total = 0;
   for (let i = 0; i < orders.length; i += 1) {
     total += orders[0].total;
@@ -33,8 +31,8 @@ export async function totalIncome(month, year) {
   return total;
 }
 
-export async function inconsistentBalance(month, year) {
-  const orders = await monthDates(month, year);
+export function inconsistentBalance(month, year) {
+  const orders = monthDates(month, year);
   const output = [];
   let prodPrices = 0;
   for (let order = 0; order < orders.length; order += 1) {
